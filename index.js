2dfa5af3ed249acc158305e520e736e5712aa5c6
wahidgoni17 <imwahidgoni17@gmail.com>
2023-05-31 23:39:36 +0600

    update menu collection

--- index.js
+++ index.js
@@ -113,6 +113,17 @@ async function run() {
       const result = await menuCollection.find().toArray();
       res.send(result);
     });
+    app.post('/menu', verifyJWT, verifyAdmin, async(req, res) =>{
+      const newItem = req.body
+      const result = await menuCollection.insertOne(newItem)
+      res.send(result)
+    })
+    app.delete('/menu/:id',verifyJWT, verifyAdmin, async(req, res)=>{
+      const id = req.params.id
+      const query = { _id: new ObjectId(id) };
+      const result = await menuCollection.deleteOne(query);
+      res.send(result);
+    })
 
     // review apis
     app.get("/review", async (req, res) => {
