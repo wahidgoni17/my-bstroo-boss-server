63ba8ccba7637b5289fde0c829dcd93f02cff6db
wahidgoni17 <imwahidgoni17@gmail.com>
2023-08-05 23:10:06 +0600

    modified vercel

--- index.js
+++ index.js
@@ -42,7 +42,7 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
     const usersCollection = client.db("bistroodb").collection("users");
     const menuCollection = client.db("bistroodb").collection("menu");
     const reviewCollection = client.db("bistroodb").collection("reviews");
